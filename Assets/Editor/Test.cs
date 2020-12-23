a5e3730bbdbf18d1b318f1a1ad4eb6ef7d1fbb0a
peterTrinh <trinhquanghut@gmail.com>
2020-12-23 12:00:54 +0700

    Add test script

---
+++ Assets/Editor/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [MenuItem("Build/BuildApk")]
+    public static void BuildApk()
+    {
+        TestBuild.BuildApk();
+    }
+
+}
---
+++ Assets/Editor/TestBuild.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class TestBuild : MonoBehaviour
+{
+    
+
+    public static void BuildApk()
+    {
+        var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Android";
+        var outputPath = Path.Combine(outdir, Application.productName + ".apk");
+        // Folder processing
+        if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
+        if (File.Exists(outputPath)) File.Delete(outputPath);
+
+        // Start project one-click packaging
+        string[] scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        UnityEditor.BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+        if (File.Exists(outputPath))
+        {
+            Debug.Log("Build Success :" + outputPath);
+        }
+        else
+        {
+            Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+        }
+    }
+
+}
